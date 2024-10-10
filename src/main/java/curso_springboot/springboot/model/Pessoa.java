7a96deb0438aba09c48e80c67793b00b46bce370
thiago <thiagomatheus2028@gmail.com>
2024-10-10 13:43:08 -0300

    Remoção de pessoa com seus telefones amarrados.

--- src/main/java/curso_springboot/springboot/model/Pessoa.java
+++ src/main/java/curso_springboot/springboot/model/Pessoa.java
@@ -20,7 +20,7 @@ public class Pessoa implements Serializable {
     private int idade;
 
 
-    @OneToMany(mappedBy = "pessoa")
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Telefone> telefones;
 
     public List<Telefone> getTelefones() {
